925f923db8528d6901bb980dd08c654999233666
Dergi Samayoa <dergio@gmail.com>
2024-02-26 15:06:49 -0600

    Modificar controlladores de Purchase para usar el usuario logueuado

--- src/controllers/purchase.controllers.js
+++ src/controllers/purchase.controllers.js
@@ -30,9 +30,15 @@ const getAll = catchError(async(req, res) => {
 
 const create = catchError(async(req, res) => {
     const userId = req.user.id;
-    const { productId, quantity } = req.body;
-    const newBody = { userId, productId, quantity };
-    const result = await Purchase.create(newBody);
+    const cart = await Cart.findAll({
+        where: { userId }, 
+        raw: true,
+        attributes: ["quantity", "productId", "userId"]
+    });
+    if(!cart) return res.sendStatus(404);
+    const result = await Purchase.bulkCreate(cart);
+    if(!result) return res.sendStatus(404);
+    await Cart.destroy({ where: { userId }  });
     return res.status(201).json(result);
 });
 
